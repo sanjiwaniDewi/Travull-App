c570eba5f0e5e2e23b3a22bfc25497ecd8420ac4
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-02 14:30:53 +0900

    feat: login page

---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import LoginForm from "@/components/LoginForm";
+
+export default function LoginPage() {
+    return (
+        <>
+            <h1>ini page login</h1>
+            <LoginForm />
+        </>
+    );
+}
