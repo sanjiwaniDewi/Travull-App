73ccd14b4d2becd23fbf21eeb01eee3461eea3c9
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-05 09:02:50 +0900

    add image and user reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore, Tuple } from "@reduxjs/toolkit";
 import authReducer from "@/redux/features/auth/authSlice";
+import imageReducer from "@/redux/features/upload/imageSlice";
 
+import userReducer from "@/redux/features/user/userSlice";
 export const makeStore = () => {
     return configureStore({
         devTools: true,
         reducer: {
             auth: authReducer,
+            image: imageReducer,
+            user: userReducer,
         },
     });
 };
