84d4a61965cfea7c239988882d9e9b31caeea679
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-05 09:02:04 +0900

    add userslice

---
+++ src/redux/features/user/userSlice.js
@@ -0,0 +1,58 @@
+import { API_KEY, BASE_API } from "@/API/api";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    id: "",
+    name: "",
+    email: "",
+    role: "",
+    profilePictureUrl: "",
+    phoneNumber: "",
+};
+
+export const fatchUserLogged = createAsyncThunk(
+    "user/fatchUserLogged",
+    async (payload, thunkAPI) => {
+        const { rejectWithValue, dispatch } = thunkAPI;
+
+        try {
+            const token = localStorage.getItem("access_token");
+
+            if (token) {
+                const res = await axios.get(`${BASE_API}/user`, {
+                    headers: {
+                        apiKey: API_KEY,
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                return dispatch({
+                    type: "user/getLoggedUser",
+                    payload: res.data.data,
+                });
+            }
+        } catch (err) {}
+    }
+);
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        getLoggedUser(state, action) {
+            state.id = action.payload.id;
+            state.name = action.payload.name;
+            state.email = action.payload.email;
+            state.role = action.payload;
+            state.profilePictureUrl = action.payload.profilePictureUrl;
+            state.phoneNumber = action.payload.phoneNumber;
+        },
+        updateUserRole(state, action) {
+            state.role = action.payload;
+        },
+    },
+});
+
+export const { getLoggedUser, updateUserRole } = userSlice.actions;
+export default userSlice.reducer;
