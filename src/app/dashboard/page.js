178e8496c4f784020efce4a0048d16b2f2325b6b
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-05 17:50:28 +0900

    feat: dashboard page

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,10 @@
+import AllUser from "@/components/AllUser";
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <p>ini Dashboard</p>
+            <AllUser />
+        </div>
+    );
+}
