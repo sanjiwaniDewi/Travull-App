daf23eca83ad93e8606a3cd649a48346e75e649f
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-06 20:58:58 +0900

    new userDropdon component

---
+++ src/components/UserDropdownMenu.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import Link from "next/link";
+
+export default function DropdownMenu({ dataUser, handleLogout }) {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    return (
+        <>
+            <button
+                id="dropdownMenuIconButton"
+                data-dropdown-toggle="dropdownDots"
+                type="button"
+                onClick={toggleDropdown}
+                className=" flex self-center"
+            >
+                <img
+                    src={dataUser.profilePictureUrl}
+                    className="w-8 h-8 rounded-full object-cover"
+                    alt="User Avatar"
+                />
+            </button>
+
+            <div
+                id="dropdownDots"
+                className={
+                    showDropdown
+                        ? "absolute mt-1 2xl:right-32 xl:right-4 lg:right:16 md:right-6 right-0 z-10 show bg-black divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+                        : "hidden"
+                }
+            >
+                <ul
+                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdownMenuIconButton"
+                >
+                    <li>
+                        <Link
+                            href="/profile"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                            Profile
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/dashboard"
+                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                            Dashboard
+                        </Link>
+                    </li>
+                </ul>
+                <div className="py-2">
+                    <button
+                        className="block px-4 py-1 text-sm text-gray-700
+                    hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200
+                    dark:hover:text-white"
+                        onClick={handleLogout}
+                    >
+                        Logout
+                    </button>
+                </div>
+            </div>
+        </>
+    );
+}
