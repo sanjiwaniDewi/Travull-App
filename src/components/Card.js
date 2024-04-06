2660a1fac79411bdc7303d8958fe55a1f3552d90
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-06 20:55:09 +0900

    new card component

---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+export default function Card({ children }) {
+    return (
+        <div className="block max-w-fit p-6 bg-white border border-gray-200 rounded-lg lg:mx-auto mx-8">
+            {children}
+        </div>
+    );
+}
