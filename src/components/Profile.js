7a1cc802a635e0409783f70a3e4a10f6cf9564b4
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-06 20:58:36 +0900

    styleing profile in profilepage

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,13 @@
 export default function Profile({ userData }) {
     console.log(userData);
     return (
-        <div key={userData.id}>
-            <img src={userData.profilePictureUrl} />
-            <p>Nama: {userData.name}</p>
+        <div key={userData.id} className="flex flex-col gap-y-1">
+            <img
+                src={userData.profilePictureUrl}
+                className="rounded-full w-52 h-52 "
+                alt="User Avatar"
+            />
+            <p className="text-3xl font-bold mb-1 mt-3">{userData.name}</p>
             <p>Email: {userData.email}</p>
             <p>Phone Number: {userData.phoneNumber}</p>
             <p>Role: {userData.role}</p>
