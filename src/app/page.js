f09f1c644549743e66af0c3430b69b932e2635ef
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-06 20:44:16 +0900

    insert layout in homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Layout from "@/components/Layout";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+        <Layout>
+            <div className="flex min-h-screen flex-col items-center justify-between p-24"></div>
+        </Layout>
     );
 }
