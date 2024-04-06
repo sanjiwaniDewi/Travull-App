a34154ac86c29e57250f616093ae4ed7d6582dfe
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-06 20:47:44 +0900

    style: insert style

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,9 +4,8 @@ import LoginForm from "@/components/LoginForm";
 
 export default function LoginPage() {
     return (
-        <>
-            <h1>ini page login</h1>
+        <div className="flex justify-center items-center h-screen">
             <LoginForm />
-        </>
+        </div>
     );
 }
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -5,47 +5,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { uploadImage } from "@/redux/features/upload/imageSlice";
 import RegisterForm from "@/components/RegisterForm";
 
+
 export default function RegisterPage() {
-    const [image, setImage] = useState("/Avatar-Image.png");
-
-    // const imageUploadRef = useRef();
-
-    const dispatch = useDispatch();
-
-    // const { imageUrl } = useSelector((store) => store.image);
-
-    // const handleUploadImage = (e) => {
-    //     e.preventDefault();
-    //     const uploadedImage = imageUploadRef.current.files[0];
-    //     const formData = new FormData();
-    //     formData.append("image", uploadedImage);
-    //     dispatch(uploadImage(formData));
-    //     setImage(imageUrl);
-    // };
-
-    // const handleImageChange = () => {
-    //     if (imageUrl) setImage(imageUrl);
-    // };
-
-    // useEffect(() => {
-    //     handleImageChange();
-    // }, [imageUrl]);
-
     return (
-        <>
-            <h1>ini page Register</h1>
-            {/* <img src={image} alt="avatar" />
-            <form onSubmit={handleUploadImage} encType="multipart/form-data">
-                <input type="file" name="image" ref={imageUploadRef} />
-
-                <button
-                    type="submit"
-                    className="bg-slate-50 px-10 py-2 text-black"
-                >
-                    upload
-                </button>
-            </form> */}
+        <div className="flex justify-center items-center h-screen">
+            
             <RegisterForm />
-        </>
+        </div>
     );
 }
