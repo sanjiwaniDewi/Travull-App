d1cb57fbd7f4ee19eee6b200e52f796721bcdca8
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-11 12:29:36 +0900

    style: add tailwind style into props

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,18 +1,21 @@
 "use client";
 import { logout } from "@/redux/features/auth/authSlice";
-import { useDispatch } from "react-redux";
-export default function LogoutButton() {
+import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function LogoutButton({ isDashboard, styleButon }) {
     const dispatch = useDispatch();
+
+    const router = useRouter();
     const handleLogout = () => {
         dispatch(logout());
+        console.log(isDashboard);
+        if (isDashboard) {
+            router.push("/");
+        }
     };
     return (
-        <button
-            className="block px-4 py-1 text-sm text-gray-700
-                    hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200
-                    dark:hover:text-white"
-            onClick={handleLogout}
-        >
+        <button className={styleButon} onClick={handleLogout}>
             Logout
         </button>
     );
