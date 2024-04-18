41c9bce3fcf4d29df44a9210c6b3986ea44c2d3c
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-18 20:54:28 +0900

    store  delete and getAllBanners api

--- src/API/banner.js
+++ src/API/banner.js
@@ -1,14 +1,35 @@
 "use client";
 import axios from "axios";
 import { BASE_API, API_KEY } from "./api";
-import { token, headers } from "./axios";
+import apiInstance from "./axios";
 
-export async function deleteBanner(id) {
+export async function createBannerAPI(payload) {
+    const { token, headers } = apiInstance();
     if (token) {
-        const res = await axios.delete(`${BASE_API}/delete-banner/${id}`, {
+        const res = await axios.post(`${BASE_API}/create-banner`, payload, {
             headers: headers,
         });
         console.log(res.data);
         return res.data;
+    }
+}
+export async function deleteBannerAPI(id) {
+    const { token, headers } = apiInstance();
+    if (token) {
+        const res = await axios.delete(`${BASE_API}/delete-banner/${id}`, {
+            headers: headers,
+        });
+        return res.data;
+    }
+}
+
+export async function getAllBannerAPI() {
+    const { token, headers } = apiInstance();
+    if (token) {
+        const res = await axios.get(`${BASE_API}/banners`, {
+            headers: headers,
+        });
+
+        return res.data.data;
     }
 }
