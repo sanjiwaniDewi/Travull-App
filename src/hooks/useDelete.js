af19bacd044ee3ffc8f5725d54109227f705fab9
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-19 16:08:09 +0900

    add function for delete promo, category, and activity

--- src/hooks/useDelete.js
+++ src/hooks/useDelete.js
@@ -1,6 +1,8 @@
+import { deleteActivityAPI } from "@/API/activity";
 import { BASE_API } from "@/API/api";
 import { deleteBannerAPI } from "@/API/banner";
-import { deletePromo } from "@/API/promo";
+import { deleteCategoryAPI } from "@/API/category";
+import { deletePromoAPI } from "@/API/promo";
 import axios from "axios";
 
 export default function useDelete() {
@@ -12,6 +14,37 @@ export default function useDelete() {
             console.log(err);
         }
     };
+    const deletePromoId = async (id) => {
+        try {
+            const res = deletePromoAPI(id);
+            console.log(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-    return { deleteBannerId };
+    const deleteCategoryId = async (id) => {
+        try {
+            const res = deleteCategoryAPI(id);
+            console.log(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const deleteActivityId = async (id) => {
+        try {
+            const res = deleteActivityAPI(id);
+            console.log(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return {
+        deleteBannerId,
+        deletePromoId,
+        deleteCategoryId,
+        deleteActivityId,
+    };
 }
