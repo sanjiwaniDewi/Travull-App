43f15feb34937d4262d6afc39415a1869091d003
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-19 16:07:51 +0900

    add function for create promo, category, and activity

--- src/hooks/useCreate.js
+++ src/hooks/useCreate.js
@@ -1,6 +1,8 @@
+import { createActivityAPI } from "@/API/activity";
 import { BASE_API } from "@/API/api";
 import { createBannerAPI } from "@/API/banner";
-import { deletePromo } from "@/API/promo";
+import { createCategoryAPI } from "@/API/category";
+import { createPromoAPI, deletePromo } from "@/API/promo";
 import axios from "axios";
 
 export default function useCreate() {
@@ -13,5 +15,31 @@ export default function useCreate() {
         }
     };
 
-    return { createBanner };
+    const createPromo = async (payload) => {
+        try {
+            const res = createPromoAPI(payload);
+            console.log(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const createCategory = async (payload) => {
+        try {
+            const res = createCategoryAPI(payload);
+            console.log(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    const createActivity = async (payload) => {
+        try {
+            const res = createActivityAPI(payload);
+            console.log(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return { createBanner, createPromo, createCategory, createActivity };
 }
