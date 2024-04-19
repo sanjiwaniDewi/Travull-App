2b6f319ef82dea5a92bde802637508714593c220
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-19 16:10:16 +0900

    add function for format  promo create and edit

---
+++ src/utils/handleInputForm.js
@@ -0,0 +1,24 @@
+export function handleNewPromoForm(formData, figureUrl) {
+    let imageUrl = figureUrl;
+    const title = formData.get("title");
+    const description = formData.get("description");
+
+    if (!figureUrl) {
+        imageUrl = formData.get("imageUrl");
+    }
+
+    const promoCode = formData.get("promoCode");
+    const promoDiscountPrice = formData.get("promoDiscountPrice");
+    const minClaimPrice = formData.get("minClaimPrice");
+    const termCondition = formData.get("termCondition");
+
+    return {
+        title,
+        description,
+        imageUrl,
+        promo_code: promoCode,
+        promo_discount_price: parseInt(promoDiscountPrice),
+        minimum_claim_price: parseInt(minClaimPrice),
+        terms_condition: termCondition,
+    };
+}
