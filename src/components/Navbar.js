f216126dbcfc9907623bda7ae89e297e3e065859
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-22 11:26:39 +0900

    fix: change menu in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import LoginButton from "./LoginButton";
 
 export default function Navbar() {
     return (
-        <nav className="bg-slate-400 py-4">
+        <nav className="py-3 bg-opacity-60 bg-slate-300 sticky top-0 z-50">
             <div className="container mx-auto lg:px-1 px-5 grid lg:grid-cols-5 grid-cols-3">
                 <div className="nav-logo text-slate-900 font-extrabold text-3xl content-center">
                     <h1>Travull</h1>
@@ -10,8 +10,8 @@ export default function Navbar() {
                 <div className="nav-menu flex justify-start self-center  lg:col-span-3 ">
                     <ul className="flex gap-x-10">
                         <li>Home</li>
-                        <li>About</li>
-                        <li>Service</li>
+                        <li>Promo</li>
+                        <li>Activity</li>
                     </ul>
                 </div>
                 <div className="nav-button flex justify-end self-center">
