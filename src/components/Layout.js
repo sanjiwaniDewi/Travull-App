32d3ec66aa259335d21589b0d7148d65b8a56570
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-23 00:18:18 +0900

    style: fix style

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
     return (
-        <main>
+        <main className=" bg-slate-100">
             <Navbar />
             {children}
         </main>
