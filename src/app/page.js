a8e46348db2101c7276b3f88df501fbb7fd8ab34
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-23 15:30:01 +0900

    insert section in homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 import Layout from "@/components/Layout";
+import Navbar from "@/components/Navbar";
+import ActivitySection from "@/components/homepage/activity/ActivitySection";
+import CategorySection from "@/components/homepage/category/CategorySection";
 import HeaderImage from "@/components/homepage/header/HeaderImage";
+import PromoSection from "@/components/homepage/promo/PromoSection";
 
 export default function Home() {
     return (
         <Layout>
-            <div className="flex min-h-screen flex-col items-center justify-between">
+            <article className="static w-full mb-auto pb-28 flex min-h-screen flex-col items-center justify-between top-0">
                 <HeaderImage />
-            </div>
+
+                <PromoSection />
+                <CategorySection />
+                <ActivitySection />
+            </article>
         </Layout>
     );
 }
