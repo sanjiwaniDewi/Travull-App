099baa882a82304a9194961ce1b302de26fcc6a3
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-23 15:27:52 +0900

    Implement Footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar";
+import Footer from "./layout/Footer";
 
 export default function Layout({ children }) {
     return (
         <main className=" bg-slate-100">
             <Navbar />
             {children}
+            <Footer />
         </main>
     );
 }
