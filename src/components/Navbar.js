811618cdf55ee930604560882ce443c083b69c9e
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-24 10:24:49 +0900

    add href in navbar menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import LoginButton from "./LoginButton";
 
 export default function Navbar() {
@@ -5,13 +6,19 @@ export default function Navbar() {
         <nav className=" py-3 bg-opacity-60 bg-slate-300 fixed top-0 z-50 w-full">
             <div className="container mx-auto lg:px-1 px-5 grid lg:grid-cols-5 grid-cols-3">
                 <div className="nav-logo text-slate-900 font-extrabold text-3xl content-center">
-                    <h1>Travull</h1>
+                    <Link href={"/"}>Travull</Link>
                 </div>
                 <div className="nav-menu flex justify-start self-center  lg:col-span-3 ">
                     <ul className="flex gap-x-10">
-                        <li>Home</li>
-                        <li>Promo</li>
-                        <li>Activity</li>
+                        <li>
+                            <Link href={"/promo"}>Promo</Link>
+                        </li>
+                        <li>
+                            <Link href={"/category"}>Destinasi</Link>
+                        </li>
+                        <li>
+                            <Link href={"/activity"}>Aktivitas</Link>
+                        </li>
                     </ul>
                 </div>
                 <div className="nav-button flex justify-end self-center">
