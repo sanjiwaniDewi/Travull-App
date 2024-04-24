60875b55cf358ec5740eae87c0944d98a2739197
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-24 19:57:58 +0900

    style: dashboard table

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -165,42 +165,49 @@ export default function DashboardPage() {
     }, [isDelete, isCreate, isUpdate]);
 
     return (
-        <div className="ms-10 w-full">
-            <p className="text-3xl font-bold mb-12 mt-10">Dashboard</p>
-            {latesUsers && <ShortTableUser data={latesUsers} />}
-            {lastBanner && (
-                <ShortTable
-                    data={lastBanner}
-                    title={"Kelola Banner"}
-                    detileLink={"dashboard/banners"}
-                    type={"banner"}
-                />
-            )}
-            {lastPromos && (
-                <ShortTable
-                    data={lastPromos}
-                    title={"Kelola Promo"}
-                    detileLink={"dashboard/promos"}
-                    type={"promo"}
-                />
-            )}
+        <div className=" w-full">
+            <div className="container mx-auto">
+                <h1 className="text-4xl font-bold mb-6 mt-12 ">Dashboard</h1>
+            </div>
+            <div className="w-full p-6 container mx-auto bg-white border shadow-md border-gray-200 rounded-lg mb-4 ">
+                {latesUsers && <ShortTableUser data={latesUsers} />}
+            </div>
 
-            {lastCategories && (
-                <ShortTable
-                    data={lastCategories}
-                    title={"Kelola Kategori"}
-                    detileLink={"dashboard/categories"}
-                    type={"category"}
-                />
-            )}
-            {lastActivities && (
-                <ShortTable
-                    data={lastActivities}
-                    title={"Kelola Aktivitas"}
-                    detileLink={"dashboard/activity"}
-                    type={"activity"}
-                />
-            )}
+            <div className="grid lg:grid-cols-2 grid-cols-1 container mx-auto gap-4">
+                {lastBanner && (
+                    <ShortTable
+                        data={lastBanner}
+                        title={"Kelola Banner"}
+                        detileLink={"dashboard/banners"}
+                        type={"banner"}
+                    />
+                )}
+                {lastPromos && (
+                    <ShortTable
+                        data={lastPromos}
+                        title={"Kelola Promo"}
+                        detileLink={"dashboard/promos"}
+                        type={"promo"}
+                    />
+                )}
+
+                {lastCategories && (
+                    <ShortTable
+                        data={lastCategories}
+                        title={"Kelola Kategori"}
+                        detileLink={"dashboard/categories"}
+                        type={"category"}
+                    />
+                )}
+                {lastActivities && (
+                    <ShortTable
+                        data={lastActivities}
+                        title={"Kelola Aktivitas"}
+                        detileLink={"dashboard/activity"}
+                        type={"activity"}
+                    />
+                )}
+            </div>
 
             {showModal && <DetailModal />}
         </div>
