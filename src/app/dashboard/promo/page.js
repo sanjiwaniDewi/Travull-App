dcd808d4fe0e843f4a0a01ebae10a521d3e636ec
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-25 21:05:21 +0900

    Fix:promo crud

--- src/app/dashboard/promo/page.js
+++ src/app/dashboard/promo/page.js
@@ -1,34 +1,62 @@
 "use client";
 import TabelAllData from "@/components/utils/TableAllData";
 import { useGetAllData } from "@/hooks/useGet";
+import { setData } from "@/redux/features/data/dataSlice";
+import {
+    changeCreateSatus,
+    changeDeleteSatus,
+    changeEditStatus,
+} from "@/redux/features/status/statusSilce";
 
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function PromosPage() {
-    const [promosData, setPromosData] = useState();
+    // const [promosData, setPromosData] = useState();
     const { getAllPromoData } = useGetAllData();
+
+    const { isDelete, isUpdate, isCreate } = useSelector(
+        (state) => state.status
+    );
+    const { data: promoData } = useSelector((state) => state.data);
+    const dispatch = useDispatch();
 
     const handleShowAllData = async () => {
         const res = await getAllPromoData();
-        const newPromosData = res?.map((item, index) => {
-            {
-                return {
-                    ...item,
-                    no: index + 1,
-                };
-            }
-        });
-        setPromosData(newPromosData);
+        dispatch(setData(res));
+        // setPromosData(newPromosData);
     };
 
     useEffect(() => {
         handleShowAllData();
     }, []);
+
+    const handleOnDataChange = () => {
+        if (isDelete) {
+            // handleShowAllData();
+            // setBannerData(dataBanner);
+            dispatch(changeDeleteSatus());
+        } else if (isCreate) {
+            console.log("masuk create");
+            // handleShowAllData();
+            dispatch(changeCreateSatus());
+        } else if (isUpdate) {
+            console.log("masuk update");
+            // handleShowAllData();
+            dispatch(changeEditStatus());
+        }
+    };
+
+    useEffect(() => {
+        handleOnDataChange();
+    }, [isDelete, isCreate, isUpdate]);
+
     return (
-        <div className="w-full pt-16">
-            {promosData && (
+        <div className="w-full pt-16 px-10">
+            {promoData && (
                 <TabelAllData
-                    data={promosData}
+                    data={promoData}
                     type="promo"
                     title="Tabel Promo"
                 />
--- src/components/dashboard/promo/PromoForm.js
+++ src/components/dashboard/promo/PromoForm.js
@@ -6,20 +6,36 @@ import useCreate from "@/hooks/useCreate";
 import { handlePromoForm } from "@/utils/handleInputForm";
 import { useRouter } from "next/navigation";
 import useUpdate from "@/hooks/useUpdate";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import ImageBtnOption from "@/components/utils/ImageBtnOption";
+import {
+    deleteImageUrl,
+    getImageUrl,
+} from "@/redux/features/upload/imageSlice";
+import { updateItem } from "@/redux/features/data/dataSlice";
+import {
+    changeCreateSatus,
+    changeEditStatus,
+    changeDeleteSatus,
+} from "@/redux/features/status/statusSilce";
 
 export default function PromoForm({ promoData }) {
     const [isHaveImageUrl, setIsHaveImageUrl] = useState(false);
-    const [isUploadImage, setIsUploadImage] = useState(false);
+    const [isUploadImage, setIsUploadImage] = useState(true);
     const { imageUrl } = useSelector((store) => store.image);
     const { createPromo } = useCreate();
     const { updatePromo } = useUpdate();
+
+    const dispatch = useDispatch();
+
     const router = useRouter();
     const handleHaveImageUrl = () => {
+        dispatch(deleteImageUrl());
         setIsHaveImageUrl(true);
         setIsUploadImage(false);
     };
     const handleUploadImage = () => {
+        dispatch(deleteImageUrl());
         setIsUploadImage(true);
         setIsHaveImageUrl(false);
     };
@@ -27,14 +43,30 @@ export default function PromoForm({ promoData }) {
     const handleSubmitForm = (e) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
+        const newImageUrl = formData.get("imageUrl");
 
         try {
             if (promoData) {
                 // updatePromo(promoData.id, handlePromoForm(formData));
+                if (isHaveImageUrl) {
+                    dispatch(getImageUrl(newImageUrl));
+                }
+
                 const figureUrl = imageUrl ? imageUrl : promoData?.imageUrl;
-                updatePromo(promoData.id, handlePromoForm(formData, figureUrl));
+
+                updatePromo(
+                    promoData.id,
+                    handlePromoForm(formData, figureUrl, isHaveImageUrl)
+                );
+                dispatch(updateItem(bannerData));
+
+                dispatch(changeEditStatus());
             } else {
+                if (isHaveImageUrl) {
+                    dispatch(getImageUrl(newImageUrl));
+                }
                 createPromo(handlePromoForm(formData, imageUrl));
+                dispatch(changeCreateSatus());
             }
         } catch (err) {
             console.log(err);
@@ -43,67 +75,87 @@ export default function PromoForm({ promoData }) {
     };
 
     return (
-        <div className="flex flex-col justify-center gap-2">
-            <div>
-                <button onClick={handleUploadImage}>Upload Image</button>
-                <button onClick={handleHaveImageUrl}>Have Image Url</button>
+        <div className="w-full container mx-auto flex flex-col py-16  items-center ">
+            <div className=" w-3/4 mb-4 rounded-lg">
+                <ImagePreview figureUrl={promoData?.imageUrl} />
             </div>
-            {isUploadImage && (
-                <div className="w-1/2 self-center">
-                    <ImagePreview figureUrl={promoData?.imageUrl} />
-                    <UploadImage />
-                </div>
-            )}
 
-            <form onSubmit={handleSubmitForm} className="flex flex-col">
-                <input
-                    defaultValue={promoData?.title}
-                    name="title"
-                    placeholder="title"
-                />
-                {isHaveImageUrl && (
-                    <input
-                        defaultValue={promoData?.imageUrl}
-                        name="imageUrl"
-                        placeholder="Image Url"
+            <div className=" lg:w-1/3 w-3/4 p-6 bg-white border shadow-md border-gray-200 rounded-xl">
+                <div className="flex flex-col justify-center items-center gap-y-4">
+                    <ImageBtnOption
+                        haveImageUrl={handleHaveImageUrl}
+                        uploadImage={handleUploadImage}
                     />
-                )}
+                    {isUploadImage && (
+                        <div className="w-11/12 self-center">
+                            <UploadImage />
+                        </div>
+                    )}
 
-                <input
-                    defaultValue={promoData?.promo_code}
-                    name="promoCode"
-                    placeholder="Promo Code"
-                />
-                <textarea
-                    defaultValue={promoData?.description}
-                    name="description"
-                    placeholder="Description"
-                ></textarea>
-                <textarea
-                    defaultValue={promoData?.terms_condition}
-                    name="termCondition"
-                    placeholder="termCondition"
-                ></textarea>
-                <input
-                    defaultValue={promoData?.promo_discount_price}
-                    type="number"
-                    name="promoDiscountPrice"
-                    placeholder="Promo Discount Price"
-                />
-                <input
-                    defaultValue={promoData?.minimum_claim_price}
-                    type="number"
-                    name="minClaimPrice"
-                    placeholder="Minimum Claim Price"
-                />
+                    <form
+                        onSubmit={handleSubmitForm}
+                        className="self-center w-11/12 flex flex-col gap-y-1"
+                    >
+                        <input
+                            defaultValue={promoData?.title}
+                            name="title"
+                            className="w-full focus:outline-slate-600 py-3 mb-2 bg-slate-100 px-3 rounded-xl outline outline-1 outline-slate-300 "
+                            placeholder="title"
+                        />
+                        {isHaveImageUrl && (
+                            <input
+                                defaultValue={promoData?.imageUrl}
+                                name="imageUrl"
+                                className="w-full focus:outline-slate-600 py-3 mb-2 bg-slate-100 px-3 rounded-xl outline outline-1 outline-slate-300 "
+                                placeholder="Image Url"
+                            />
+                        )}
 
-                <button
-                    type="submit"
-                    disabled={!isHaveImageUrl && !isUploadImage}
-                >
-                    Submit
-                </button>
-            </form>
+                        <input
+                            defaultValue={promoData?.promo_code}
+                            name="promoCode"
+                            className="w-full focus:outline-slate-600 py-3 mb-2 bg-slate-100 px-3 rounded-xl outline outline-1 outline-slate-300 "
+                            placeholder="Promo Code"
+                        />
+                        <textarea
+                            defaultValue={promoData?.description}
+                            name="description"
+                            className="w-full focus:outline-slate-600 py-3 mb-2 bg-slate-100 px-3 rounded-xl outline outline-1 outline-slate-300 "
+                            placeholder="Description"
+                        ></textarea>
+                        <textarea
+                            defaultValue={promoData?.terms_condition}
+                            name="termCondition"
+                            className="w-full focus:outline-slate-600 py-3 mb-2 bg-slate-100 px-3 rounded-xl outline outline-1 outline-slate-300 "
+                            placeholder="termCondition"
+                        ></textarea>
+                        <input
+                            defaultValue={promoData?.promo_discount_price}
+                            type="number"
+                            name="promoDiscountPrice"
+                            className="w-full focus:outline-slate-600 py-3 mb-2 bg-slate-100 px-3 rounded-xl outline outline-1 outline-slate-300 "
+                            placeholder="Promo Discount Price"
+                        />
+                        <input
+                            defaultValue={promoData?.minimum_claim_price}
+                            type="number"
+                            name="minClaimPrice"
+                            className="w-full focus:outline-slate-600 py-3 mb-2 bg-slate-100 px-3 rounded-xl outline outline-1 outline-slate-300 "
+                            placeholder="Minimum Claim Price"
+                        />
+
+                        <div className="flex justify-center">
+                            <button
+                                type="submit"
+                                disabled={!isHaveImageUrl && !isUploadImage}
+                                className="bg-slate-500 text-sm text-white py-2  px-4 rounded-2xl w-1/4"
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
