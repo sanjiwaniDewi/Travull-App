93bbdf438a5610e7f6c09f10c36fd5cbc266d1a5
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-26 06:36:01 +0900

    style: add styling

--- src/components/auth/register/RegisterForm.js
+++ src/components/auth/register/RegisterForm.js
@@ -1,16 +1,18 @@
+"use client";
 import { useState } from "react";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import Image from "next/image";
-import { register } from "@/redux/features/auth/authSlice";
+import { register, setErrMessage } from "@/redux/features/auth/authSlice";
 import { useRouter } from "next/navigation";
 import Card from "../../layout/Card";
 import Link from "next/link";
+import { handleRegistationForm } from "@/utils/handleInputForm";
 
 export default function RegisterForm() {
     const [image, setImage] = useState("/Avatar-Image.png");
-
+    const { errMessage } = useSelector((state) => state.auth);
     const dispatch = useDispatch();
 
     const router = useRouter();
@@ -19,91 +21,107 @@ export default function RegisterForm() {
 
         const formData = new FormData(e.currentTarget);
 
-        const name = formData.get("name");
-        const phoneNumber = formData.get("phoneNumber");
-        const email = formData.get("email");
-        const password = formData.get("password");
-        const passwordRepeat = formData.get("passwordRepeat");
-        const role = formData.get("role");
-
-        const payload = {
-            name,
-            email,
-            password,
-            passwordRepeat,
-            profilePictureUrl: image,
-            phoneNumber,
-            role,
-        };
-        // console.log(payload);
-
         try {
-            dispatch(register(payload));
-            router.push("/login");
+            const payload = handleRegistationForm(formData, image);
+
+            if (errMessage.length === 0 || payload) {
+                dispatch(register(payload));
+            }
+
+            // router.push("/login");
         } catch (err) {
             console.log(err);
         }
     };
 
+    const handleChange = () => {
+        if (errMessage.length > 0) {
+            dispatch(setErrMessage([]));
+        }
+    };
+
     return (
-        <Card>
-            <h1 className="text-3xl font-bold text-center">Register</h1>
+        <div className="card mx-auto my-auto px-4 pt-10 pb-6 rounded-5">
+            <h1 className="text-3xl font-bold text-center mb-6">Register</h1>
+            <div className="flex justify-center">
+                <p className="text-sm w-96 text-center text-red-600  ">
+                    {" "}
+                    {Array.isArray(errMessage)
+                        ? errMessage.join(" ")
+                        : errMessage}
+                </p>
+            </div>
 
             <form
                 onSubmit={handleSubmitForm}
-                className="flex flex-col  w-96 gap-y-2 text-black mt-8"
+                className="flex flex-col  gap-y-1 text-black "
+                onChange={handleChange}
             >
+                <label className="text-sm">Name</label>
                 <input
                     type="text"
                     name="name"
-                    placeholder="name"
-                    className="bg-slate-100 px-2 py-4 rounded-xl focus:outline-none"
+                    placeholder="Nama"
+                    className="bg-slate-100 px-3 py-3 rounded-xl focus:outline-none"
                 />
-
+                <label className="text-sm ">Phone Number</label>
                 <input
                     type="text"
                     name="phoneNumber"
-                    placeholder="phone number"
-                    className="bg-slate-100 px-2 py-4 rounded-xl focus:outline-none"
+                    placeholder="Phone number"
+                    className="bg-slate-100 px-3 py-3 rounded-xl focus:outline-none"
                 />
+
+                <label className="text-sm">Email</label>
                 <input
                     type="text"
                     name="email"
-                    placeholder="email"
-                    className="bg-slate-100 px-2 py-4 rounded-xl focus:outline-none"
+                    placeholder="Email"
+                    className="bg-slate-100 px-3 py-3 rounded-xl focus:outline-none"
                 />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                    className="bg-slate-100 px-2 py-4 rounded-xl focus:outline-none"
-                />
-                <input
-                    type="password"
-                    name="passwordRepeat"
-                    placeholder="repeat password"
-                    className="bg-slate-100 px-2 py-4 rounded-xl focus:outline-none"
-                />
+                <div className="grid grid-cols-2 gap-2">
+                    <div className="flex flex-col gap-y-1">
+                        <label className="text-sm">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            className="bg-slate-100 px-3 py-3 rounded-xl focus:outline-none"
+                        />
+                    </div>
+
+                    <div className="flex flex-col gap-y-1">
+                        <label className="text-sm">Repeat Password</label>
+                        <input
+                            type="password"
+                            name="passwordRepeat"
+                            placeholder="Repeat password"
+                            className="bg-slate-100 px-3 py-3 rounded-xl focus:outline-none"
+                        />
+                    </div>
+                </div>
+
+                <label className="text-sm">Role</label>
                 <select
                     name="role"
-                    className="bg-slate-100 px-2 py-4 rounded-xl focus:outline-none"
+                    className="bg-slate-100 px-3 py-3 rounded-xl focus:outline-none"
                 >
                     <option value="admin">Admin</option>
                     <option value="User">User</option>
                 </select>
                 <button
                     type="submit"
-                    className="text-slate-50 mt-4  w-96 px-2 py-3 rounded-xl font-bold bg-slate-500"
+                    className="text-slate-50 mt-4 px-2 py-3 rounded-xl font-bold bg-slate-500"
                 >
                     Register
                 </button>
             </form>
             <div className="mt-4 flex justify-center gap-3">
-                <p>Have an account already?</p>
+                <p>Sudah punya akun?</p>
                 <Link href="/login" className="text-blue-700 underline">
                     Login
                 </Link>
             </div>
-        </Card>
+        </div>
     );
 }
