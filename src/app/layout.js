13ca62ed274f14f948eb9e2163ced2b376d9e68c
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-27 06:55:33 +0900

    Add new config font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,21 @@
-import { Inter } from "next/font/google";
+import { Inter, Titillium_Web } from "next/font/google";
 // import "bootstrap/dist/css/bootstrap.css";
 import "./globals.css";
 import StoreProvider from "@/redux/StoreProvider";
 // import BootstrapClient from "@/components/utils/BootstrapClient.js";
 import Navbar from "@/components/layout/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-inter",
+});
+
+const titillium_Web = Titillium_Web({
+    subsets: ["latin"],
+    weight: ["300", "400", "700"],
+    variable: "--font-titi",
+});
 
 export const metadata = {
     title: "Travull App",
@@ -18,11 +28,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <StoreProvider>
-            <html lang="en">
-                <body
-                    className={inter.className}
-                    suppressHydrationWarning={true}
-                >
+            <html
+                lang="en"
+                className={`${inter.variable} ${titillium_Web.variable}`}
+            >
+                <body className="" suppressHydrationWarning={true}>
                     {children}
                     {/* <BootstrapClient /> */}
                 </body>
