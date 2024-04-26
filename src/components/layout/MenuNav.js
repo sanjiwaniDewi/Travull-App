fdaaa0503be5e84b05bffd253611867465160036
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-27 06:58:05 +0900

    Feat: Responcive Navbar HomePage

---
+++ src/components/layout/MenuNav.js
@@ -0,0 +1,45 @@
+"use client";
+import { useState } from "react";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import { useSelector } from "react-redux";
+import Link from "next/link";
+
+export default function MenuNav() {
+    const { isLogin } = useSelector((store) => store.auth);
+
+    return (
+        <div className="w-full mt-6 pb-4 ">
+            <ul className="flex flex-col gap-y-4 font-semibold">
+                {isLogin && (
+                    <div>
+                        <li className="">
+                            <Link href={"/Profile"}>Profil</Link>
+                        </li>
+                        <li className="">
+                            <Link href={"/Dashboard"}>Dashboard</Link>
+                        </li>
+                    </div>
+                )}
+                <li className="">
+                    <Link href={"/promo"}>Promo</Link>
+                </li>
+                <li>
+                    <Link href={"/category"}>Destinasi</Link>
+                </li>
+                <li>
+                    <Link href={"/activity"}>Aktivitas</Link>
+                </li>
+                <li className="">
+                    {isLogin ? (
+                        <div>
+                            <LogoutButton />
+                        </div>
+                    ) : (
+                        <Link href="/login">Login</Link>
+                    )}
+                </li>
+            </ul>
+        </div>
+    );
+}
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,58 @@
+"use client";
 import Link from "next/link";
-import LoginButton from "../utils/LoginButton";
+import LoginButton from "./LoginButton";
+import { useState } from "react";
+import { CgMenuRight } from "react-icons/cg";
+import { GrClose } from "react-icons/gr";
+import MenuNav from "./MenuNav";
 
 export default function Navbar() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const handleShowMenu = () => {
+        setShowMenu(!showMenu);
+    };
     return (
-        <nav className=" py-3 bg-opacity-60 bg-slate-300 fixed top-0 z-50 w-full">
+        <nav
+            className={` py-3  bg-slate-300 fixed top-0 z-50 w-full ${
+                showMenu ? "bg-opacity-70" : "bg-opacity-60"
+            } `}
+        >
             <div className="container mx-auto lg:px-1 px-5 grid lg:grid-cols-5 grid-cols-3">
                 <div className="nav-logo text-slate-900 font-extrabold text-3xl content-center">
                     <Link href={"/"}>Travull</Link>
                 </div>
-                <div className="nav-menu flex justify-start self-center  lg:col-span-3 ">
-                    <ul className="flex gap-x-10">
-                        <li>
-                            <Link href={"/promo"}>Promo</Link>
-                        </li>
-                        <li>
-                            <Link href={"/category"}>Destinasi</Link>
-                        </li>
-                        <li>
-                            <Link href={"/activity"}>Aktivitas</Link>
-                        </li>
-                    </ul>
+                <div className="lg:col-span-4 col-span-2 flex justify-between expanNav">
+                    <div className="nav-menu flex justify-start self-center  font-semibold  ">
+                        <ul className="flex gap-x-10">
+                            <li className="">
+                                <Link href={"/promo"}>Promo</Link>
+                            </li>
+                            <li>
+                                <Link href={"/category"}>Destinasi</Link>
+                            </li>
+                            <li>
+                                <Link href={"/activity"}>Aktivitas</Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="nav-button flex justify-end self-center">
+                        <LoginButton />
+                    </div>
                 </div>
-                <div className="nav-button flex justify-end self-center">
-                    <LoginButton />
+                <div className="smallSizeNavbar col-span-2 ">
+                    <div className="flex justify-end  align-items-end">
+                        <button
+                            onClick={handleShowMenu}
+                            className="text-4xl font-bold"
+                        >
+                            {showMenu ? <GrClose /> : <CgMenuRight />}
+                        </button>
+                    </div>
+                </div>
+                <div class="dispay-none col-span-2 ">
+                    <div>{showMenu && <MenuNav />}</div>
                 </div>
             </div>
         </nav>
