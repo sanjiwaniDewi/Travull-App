648bed4ab3d57ac29a1df9250b07b26b60a49d11
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-26 20:00:17 +0900

    fix:comment unused bootstap setting

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 // import "bootstrap/dist/css/bootstrap.css";
 import "./globals.css";
 import StoreProvider from "@/redux/StoreProvider";
-import BootstrapClient from "@/components/utils/BootstrapClient.js";
+// import BootstrapClient from "@/components/utils/BootstrapClient.js";
 import Navbar from "@/components/layout/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
