7dc8111e088d5a719707025d5abc44b30a7a7f9c
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-26 20:02:37 +0900

    fix: move modal coponent into all user page

--- src/app/dashboard/all-user/page.js
+++ src/app/dashboard/all-user/page.js
@@ -1,9 +1,14 @@
+"use client";
 import AllUser from "@/components/dashboard/user/AllUser";
+import DetailModal from "@/components/utils/DetailModal";
+import { useSelector } from "react-redux";
 
 export default function AllUserPage() {
+    const { showModal } = useSelector((state) => state.modal);
     return (
         <div className="w-full pt-16">
             <AllUser />
+            {showModal && <DetailModal />}
         </div>
     );
 }
