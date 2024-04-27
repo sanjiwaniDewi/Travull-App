b217b8c0cae78b11f5511382613e58e8f58eeede
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-27 17:14:00 +0900

    style:fix margin

--- src/components/homepage/activity/ActivitySection.js
+++ src/components/homepage/activity/ActivitySection.js
@@ -3,7 +3,7 @@ import ActivityCountainer from "./ActivitiesContainer";
 export default function ActivitySection() {
     return (
         <div className="mt-12 container mx-auto w-full lg:px-1 px-5 ">
-            <div>
+            <div className="mb-4">
                 <h2 className="text-xl font-bold">Aktivitas Populer</h2>
                 <p className="text-sm">
                     Liburan seru dengan berbagai aktivitas menarik
--- src/components/homepage/category/CategorySection.js
+++ src/components/homepage/category/CategorySection.js
@@ -1,8 +1,8 @@
 import CategoriyCarousel from "./CategoryCarousel";
 export default function CategorySection() {
     return (
-        <div className="mt-12 container mx-auto w-full lg:px-1 px-5 ">
-            <div>
+        <div className="mt-16 container mx-auto w-full lg:px-1 px-5 ">
+            <div className="mb-4">
                 <h2 className="text-xl font-bold">Destinasi Populer</h2>
                 <p className="text-sm">Pilihan destinasi yang anda inginkan </p>
             </div>
