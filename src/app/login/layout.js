71263d9e0729511f14c86d1916a13534e9ab24a4
sajiwaniDewi <dewisanjiw@gmail.com>
2024-04-28 10:01:54 +0900

    add layout for login page

---
+++ src/app/login/layout.js
@@ -0,0 +1,9 @@
+//
+
+export default function LoginLayout({ children }) {
+    return (
+        <main className="flex relative w-full h-full loginBackground bg-opacity-50 ">
+            {children}
+        </main>
+    );
+}
