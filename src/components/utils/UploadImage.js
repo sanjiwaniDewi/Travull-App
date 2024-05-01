612e3b3543a6ab62edb885a4e61012e9e2d46980
sajiwaniDewi <dewisanjiw@gmail.com>
2024-05-01 20:46:44 +0900

    Feat : add error message image url

--- src/components/utils/UploadImage.js
+++ src/components/utils/UploadImage.js
@@ -1,59 +1,70 @@
 "use client";
-import { useState, useRef, useEffect } from "react";
-import Image from "next/image";
+import { useState, useRef } from "react";
+
 import { useDispatch, useSelector } from "react-redux";
-import { setImagesUrls, uploadImage } from "@/redux/features/upload/imageSlice";
-import ProfieImage from "../profile/ProfileImage";
+import { uploadImage } from "@/redux/features/upload/imageSlice";
+
+import Image from "next/image";
 
 export default function UploadImage({
     handleMultiple,
     customStyleBtn,
     customStyleInput,
+    customErrorStyle,
+    errorMessage,
 }) {
-    const imageUploadRef = useRef();
-    const { imageUrl } = useSelector((store) => store.image);
+    const [changeStyle, setChangeStyle] = useState("");
+    const [isUploadImage, setIsUploadImage] = useState(false);
 
+    const imageUploadRef = useRef();
     const dispatch = useDispatch();
+
+    const handleChangeImage = () => {
+        setIsUploadImage(false);
+        setChangeStyle("outline outline-1 outline-slate-300");
+    };
 
     const handleUploadImage = (e) => {
         e.preventDefault();
         const uploadedImage = imageUploadRef.current.files[0];
         const formData = new FormData();
         formData.append("image", uploadedImage);
-        try {
-            dispatch(uploadImage(formData));
-        } catch (err) {}
+
+        dispatch(uploadImage(formData));
+        setIsUploadImage(true);
+
         if (handleMultiple) {
             handleMultiple();
         }
     };
-    // const handleMultileImage = () => {
-    //     if (imageUrl) {
-    //         dispatch(setImagesUrls(imageUrl));
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     handleMultileImage();
-    // }, [imageUrl]);
 
     return (
-        <form
-            onSubmit={handleUploadImage}
-            encType="multipart/form-data"
-            className="flex flex-row justify-center gap-x-1 w-full"
-        >
-            <input
-                type="file"
-                name="image"
-                className={`self-center col-span-2 w-full py-3 px-3 rounded-xl ${customStyleInput}`}
-                ref={imageUploadRef}
-            />
-            <div className="flex justify-start">
-                <button type="submit" className={customStyleBtn}>
-                    upload
-                </button>
-            </div>
-        </form>
+        <div>
+            <form
+                onChange={handleChangeImage}
+                onSubmit={handleUploadImage}
+                encType="multipart/form-data"
+                className="flex flex-row justify-center gap-x-1 w-full"
+            >
+                <input
+                    type="file"
+                    name="image"
+                    className={`self-center col-span-2 w-full py-3 px-3 rounded-xl ${customStyleInput} ${
+                        changeStyle ? changeStyle : customErrorStyle
+                    }`}
+                    ref={imageUploadRef}
+                />
+                <div className="flex justify-start">
+                    <button type="submit" className={customStyleBtn}>
+                        upload
+                    </button>
+                </div>
+            </form>
+            {errorMessage.length !== 0 && !isUploadImage && (
+                <p className="text-red-600 text-sm flex w-11/12 text-start ">
+                    Tambahkan gambar atau upload gambar
+                </p>
+            )}
+        </div>
     );
 }
