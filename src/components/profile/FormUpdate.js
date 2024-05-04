48474ef8a4af121044209c8ad425d22fc6df9312
sajiwaniDewi <dewisanjiw@gmail.com>
2024-05-05 00:18:06 +0900

    Feat:  error edit user

--- src/components/profile/FormUpdate.js
+++ src/components/profile/FormUpdate.js
@@ -1,9 +1,28 @@
+"use client";
+import { useEffect, useState } from "react";
 export default function FormUpdate({ handleUpdateUser, dataUser }) {
+    const [errorMessage, setErrorMessage] = useState("");
+    const handleErrorMessage = () => {
+        if (dataUser?.errorMessage) {
+            setErrorMessage(dataUser?.errorMessage?.split(","));
+        }
+    };
+    console.log("eeer", errorMessage);
+
+    useEffect(() => {
+        handleErrorMessage();
+    }, [dataUser?.errorMessage]);
+
+    const onChangeForm = (e) => {
+        setErrorMessage("");
+    };
+
     return (
         <div>
             <form
                 onSubmit={handleUpdateUser}
-                className="flex flex-col md:w-96  w-full gap-y-3 mt-5"
+                className="flex flex-col w-full gap-y-3 mt-5"
+                onChange={onChangeForm}
             >
                 <label className="flex flex-col text-sm">
                     Name :
@@ -11,19 +30,34 @@ export default function FormUpdate({ handleUpdateUser, dataUser }) {
                         className="text-black bg-primary-100 mt-1 px-2 py-4 rounded-xl focus:outline-secondary-100"
                         type="text"
                         defaultValue={dataUser.name}
-                        placeholder="name"
+                        placeholder="nama"
                         name="name"
                     />
+                    {errorMessage &&
+                        errorMessage.filter((e) => e.includes("name"))
+                            .length !== 0 && (
+                            <p className="text-red-600 text-sm flex text-start ">
+                                Nama tidak boleh kosong
+                            </p>
+                        )}{" "}
                 </label>
+
                 <label className="flex flex-col text-sm">
                     Email :
                     <input
                         className="text-black bg-primary-100 mt-1 px-2 py-4 rounded-xl focus:outline-secondary-100"
-                        type="text"
+                        type="email"
                         defaultValue={dataUser.email}
                         placeholder="email"
                         name="email"
                     />
+                    {errorMessage &&
+                        errorMessage.filter((e) => e.includes("email"))
+                            .length !== 0 && (
+                            <p className="text-red-600 text-sm flex text-start ">
+                                Email tidak boleh kosong
+                            </p>
+                        )}
                 </label>
                 <label className="flex flex-col text-sm">
                     Phone Number :
