e41b709099169762a2be3e3352a1c91cc71eb24c
sajiwaniDewi <dewisanjiw@gmail.com>
2024-05-05 00:24:19 +0900

    fix: update profile page

--- src/app/profile/update-profile/page.js
+++ src/app/profile/update-profile/page.js
@@ -5,91 +5,89 @@ import Layout from "@/components/layout/Layout";
 import ProfieImage from "@/components/profile/ProfileImage";
 import UploadImage from "@/components/utils/UploadImage";
 import { deleteImageUrl } from "@/redux/features/upload/imageSlice";
-import { fatchUserLogged, updateUser } from "@/redux/features/user/userSlice";
+import {
+    fatchUserLogged,
+    setSuccessUpdate,
+    updateUser,
+} from "@/redux/features/user/userSlice";
 import { redirect, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeEditStatus } from "@/redux/features/status/statusSilce";
+import { checkIsEmptyInput } from "@/utils/handleFormatData";
 
 export default function UpdateProfilePage() {
-    const dispatch = useDispatch();
-    const router = useRouter();
     const dataUser = useSelector((store) => store.user);
     const { imageUrl } = useSelector((store) => store.image);
-    const { isUpdate } = useSelector((store) => store.status);
-
-    // const handleUserData = () => {
-    //     dispatch(fatchUserLogged());
-    // };
-
-    // useEffect(() => {
-    //     handleUserData();
-    // }, []);
+    const dispatch = useDispatch();
+    const router = useRouter();
 
     const handleUpdateUser = (e) => {
         e.preventDefault();
 
         const formData = new FormData(e.currentTarget);
-        const name = formData.get("name");
-        const phoneNumber = formData.get("phoneNumber");
-        const email = formData.get("email");
+        const name = checkIsEmptyInput(formData.get("name"));
+        const phoneNumber = checkIsEmptyInput(formData.get("phoneNumber"));
+        const email = checkIsEmptyInput(formData.get("email"));
 
         const user = {
             name,
             email,
             phoneNumber,
             profilePictureUrl:
-                imageUrl !== "" ? imageUrl : dataUser.profilePictureUrl,
+                imageUrl !== "" && imageUrl !== null
+                    ? imageUrl
+                    : dataUser.profilePictureUrl,
         };
 
         try {
             dispatch(updateUser(user));
-            dispatch(changeEditStatus());
-            // redirect("/profile");
-        } catch (err) {
-            // console.log(err);
-        }
+        } catch (err) {}
         dispatch(deleteImageUrl());
     };
-    const handleRedirect = () => {
-        if (isUpdate) {
-            dispatch(changeEditStatus());
-            router.push("/profile");
-        }
+    // const handleRedirect = () => {
+    //     router.push("/profile");
+    // };
+    // useEffect(() => {
+    //     handleRedirect();
+    // }, []);
+
+    const redirectOnSuccessSubmit = () => {
+        dispatch(setSuccessUpdate(false));
+        if (dataUser.successUpdate) router.push("/profile");
     };
+
     useEffect(() => {
-        handleRedirect();
-    }, [isUpdate]);
+        redirectOnSuccessSubmit();
+    }, [dataUser.successUpdate]);
 
     return (
-        <Layout>
-            <div className="container mx-auto w-full h-full py-52">
-                <div className="flex justify-center items-center w-full ">
-                    <Card>
-                        <div className="flex md:flex-row flex-col md:gap-10 gap:3 p-4">
-                            <div className="flex flex-col w-full">
-                                <ProfieImage
-                                    image={dataUser.profilePictureUrl}
+        <div className="container mx-auto w-full h-full py-52">
+            <div className="flex justify-center items-center w-full ">
+                <div className="block p-4 bg-white border border-gray-200 rounded-lg lg:mx-auto mx-5 lg:w-2/3">
+                    <div className="grid lg:grid-cols-2 grid-cols-1 gap-x-6 p-4">
+                        <div className="flex flex-col w-full">
+                            <ProfieImage image={dataUser.profilePictureUrl} />
+                            <div className="flex justify-center text-sm">
+                                <UploadImage
+                                    customStyleBtn={
+                                        "  w-full px-2 py-3 rounded-xl font-semibold text-sm  bg-secondary-100 text-primary-200"
+                                    }
+                                    customStyleInput={
+                                        "bg-primary-100  px-2 py-4 rounded-xl focus:outline-secondary-100"
+                                    }
                                 />
-                                <div className="flex justify-center text-sm">
-                                    <UploadImage
-                                        customStyleBtn={
-                                            "  w-full px-2 py-3 rounded-xl font-semibold text-sm  bg-secondary-100 text-primary-200"
-                                        }
-                                        customStyleInput={
-                                            "bg-primary-100  px-2 py-4 rounded-xl focus:outline-secondary-100"
-                                        }
-                                    />
-                                </div>
                             </div>
+                        </div>
+                        <div>
                             <FormUpdate
                                 handleUpdateUser={handleUpdateUser}
                                 dataUser={dataUser}
                             />
                         </div>
-                    </Card>
+                    </div>
                 </div>
             </div>
-        </Layout>
+        </div>
     );
 }
