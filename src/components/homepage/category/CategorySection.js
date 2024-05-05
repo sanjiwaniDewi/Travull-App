d134edb5ba26447b41ed7312faa9cbc60cfa108d
sajiwaniDewi <dewisanjiw@gmail.com>
2024-05-05 14:50:15 +0900

    fix: change title to link

--- src/components/homepage/category/CategorySection.js
+++ src/components/homepage/category/CategorySection.js
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import CategoriyCarousel from "./CategoryCarousel";
+import { MdNavigateNext } from "react-icons/md";
 export default function CategorySection() {
     return (
         <div className="mt-16 container mx-auto w-full lg:px-1 px-5 ">
-            <div className="mb-4">
-                <h2 className="text-xl font-bold">Destinasi Populer</h2>
+            <div className="mb-4 ">
+                <div className="flex gap-x-1 text-xl font-bold cursor-pointer hover:text-secondary-300 hover:opacity-60">
+                    <Link href="/category" className=" ">
+                        Destinasi Populer{" "}
+                    </Link>
+                    <span className="text-3xl">
+                        <MdNavigateNext />
+                    </span>
+                </div>
+
                 <p className="text-sm">Pilihan destinasi yang anda inginkan </p>
             </div>
             <div>
